9d0ee196720479ffd11da387fef973509654d4fa
Andrew Novoselac <andrew.novoselac@shopify.com>
2022-07-12 09:05:06 -0400

    Make class more class like

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,27 +4,39 @@ class Bottles
   end
 
   def verse(number)
-    Bottle.new.verse(number)
+    Bottle.new(number).verse
   end
 end
 
 class Bottle
-  def verse(number)
-    "#{remainder(number)} #{bottle(number)} of beer on the wall, " +
-      "#{remainder(number)} #{bottle(number)} of beer.\n" +
-      "Take #{take(number)} down and pass it around, " +
-      "#{remainder(number - 1)} #{bottle(number - 1)} of beer on the wall.\n"
+  attr_reader :index
+
+  def initialize(index)
+    @index = index
   end
 
-  def bottle(number)
-    number == 1 ? "bottle" : "bottles"
+  def verse
+    "#{remainder} #{bottle} of beer on the wall, " +
+      "#{remainder} #{bottle} of beer.\n" +
+      "Take #{take} down and pass it around, " +
+      "#{next_bottle} of beer on the wall.\n"
   end
 
-  def remainder(number)
-    number == 0 ? "no more" : number
+  def bottle
+    index == 1 ? "bottle" : "bottles"
   end
 
-  def take(number)
-    number == 1 ? "it" : "one"
+  def remainder
+    index == 0 ? "no more" : index
+  end
+
+  def take
+    index == 1 ? "it" : "one"
+  end
+
+  def next_bottle
+    return "no more bottles" if index - 1 == 0
+
+    index - 1 == 1 ? "1 bottle" : "#{index - 1} bottles"
   end
 end
